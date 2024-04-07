65c6fab5f32ad8d65af09ceca2629b0c10e74521
Gabriel Pacheco Lopes <gabrielplopes7@hotmail.com>
2024-04-07 14:07:25 -0300

    Versão atualizada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.wcorp.w_corpandroidpedido"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         signingConfig = signingConfigs.getByName("release")
